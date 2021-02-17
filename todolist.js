c14d5dee6c2089804f84cf4775d34e5c8ddca798
dongkey1198 <utdhkim94@gmail.com>
2021-02-17 15:57:14 +0900

    completed: choose card color/delete card/ delete all card functions

--- todolist.js
+++ todolist.js
@@ -1,6 +1,16 @@
 const inputBox = document.querySelector(".input-card .input input");
 const addButton = document.querySelector(".input-card .input button");
 const colorLists = document.querySelector(".input-card .colors");
+const todoList = document.querySelector(".todoList");
+const deleteAllBtn = document.querySelector(".footer button");
+const taskCount = document.querySelector(".footer span");
+
+const colors = ["#ff616f", "#ff79b0","#ff79ff", "#9effff", "#e7ff8c", "#ffa06d"];
+let color = colors[0];
+let arr;
+
+
+showTask();
 
 // onkeyup은 키보드를 발생하는 이벤트이다.
 // 사용자가 아무런 데이터를 inputbox에 입력하지 않았을경우 addButton을 비활성화
@@ -15,36 +25,69 @@ inputBox.onkeyup = function(){
     }
 };
 
-// onmouseover 이벤트를 이용하여 li 테그위에 마우스가  위치하였을때 check icon이 화면에 출력될수
-//있도록 사용함
-// colorLists.onmouseover = function(e){
-    
-//     if(e.target.nodeName !== "LI" ){
-//         return;
-//     }
-//     let checkIcon = `<i class="fas fa-check" style="color:#80e27e";></i>`;
-//     e.target.innerHTML = checkIcon;
-// };
-
-// // onmouseout 이벤트를 이용하여 li 태그위에 마우스가 없을경우 check icon이 화면에서 사라지게 함
-// colorLists.onmouseout = function(e){
-//     if(e.target.nodeName !== "LI" ){
-//         return;
-//     }
-//     let checkIcon = e.target.querySelector("svg");
-//     e.target.removeChild(checkIcon);
-// };
-
 colorLists.onclick = function(e) {
     if(e.target.nodeName !== "LI" ){
         return;
     }
-
-    if(e.target.hasChildNodes()){
-        let ch = e.target.querySelector("svg");
-        console.log(ch);
-    }
-    e.target.style.border = "3px solid #80e27e"
-    let checkIcon = `<i class="fas fa-check" style="color:#80e27e";></i>`;
-    e.target.innerHTML = checkIcon;
+    inputBox.style.backgroundColor = colors[e.target.value];
+    color = colors[e.target.value];
 };
+
+deleteAllBtn.onclick= function(){
+    arr = [];
+    localStorage.setItem("todo", JSON.stringify(arr)); // transfroming js object into a json string
+    showTask();
+};
+
+addButton.onclick = function(){
+    let inputData = inputBox.value;
+    let getLocalStorage = localStorage.getItem("todo");
+    if(getLocalStorage === null){
+        arr = [];
+    }
+    else{
+        arr = JSON.parse(getLocalStorage);
+    }
+
+    let tmp = [inputData, color];
+    arr.push(tmp);
+    console.log(arr);
+
+    localStorage.setItem("todo", JSON.stringify(arr));
+    showTask();
+    inputBox.value = "";
+    inputBox.style.backgroundColor = "#fff";
+    color = colors[0];
+};
+
+function showTask(){
+    let getLocalStorage = localStorage.getItem("todo");
+    if(getLocalStorage === null){
+        arr = [];
+    }
+    else{
+        arr = JSON.parse(getLocalStorage);
+    }
+
+    let newLiTag ='';
+    arr.forEach((element, index) => {
+        newLiTag += `<li style ="background-color:${element[1]};">${index+1}. ${element[0]}<span onclick="deleteTask(${index});"><i class="fas fa-trash"></i></span></li>`;
+    });
+    
+    todoList.innerHTML = newLiTag;
+    
+    countTask();
+};
+
+function countTask(){
+    taskCount.textContent = `오늘 할일이 ${arr.length}개 남았습니다!!!`
+};
+
+function deleteTask(index){
+    let getLocalStorage = localStorage.getItem("todo");
+    arr = JSON.parse(getLocalStorage);
+    arr.splice(index, 1);
+    localStorage.setItem("todo", JSON.stringify(arr));
+    showTask();
+}
+
